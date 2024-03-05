206283773370e004af8970d927f18f3144af4027
Rafael Nascimento Martins, Indra Company <39777136+rafaelnascimentomartins@users.noreply.github.com>
2024-03-05 13:16:21 -0300

    subindo ajuste

--- index.js
+++ index.js
@@ -11,7 +11,14 @@
   
     _public.DownloadApk = function () {
       setTimeout(function() {
-        location.href = window.origin + "/downloads/app-release.apk";
+        var file_path = 'downloads/app-release.apk';
+        var a = document.createElement('A');
+        a.href = file_path;
+        a.download = file_path.substr(file_path.lastIndexOf('/') + 1);
+        document.body.appendChild(a);
+        a.click();
+        document.body.removeChild(a);
+        //location.href = window.origin + "/downloads/app-release.apk";
       }, 4000);
       setTimeout(function() {
         $('#baixandoApk').addClass('none');
