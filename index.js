91db2beab69d23f02c0d9982e06e467b0b617c2a
Rafael Nascimento Martins, Indra Company <39777136+rafaelnascimentomartins@users.noreply.github.com>
2024-03-05 12:46:59 -0300

    commit
    commit

---
+++ index.js
@@ -0,0 +1,23 @@
+'use strict';
+
+(function () {
+  var _private = {};
+  var _public = {};
+
+    $(function () {
+      if(!$("#baixandoApk").hasClass('none'))
+        _public.DownloadApk();
+    });
+  
+    _public.DownloadApk = function () {
+      setTimeout(function() {
+        location.href = "/downloads/app-release.apk";
+      }, 4000);
+      setTimeout(function() {
+        $('#baixandoApk').addClass('none');
+        $('#apkBaixado').css('display','block');
+      }, 5000);
+
+    };
+    
+})();
